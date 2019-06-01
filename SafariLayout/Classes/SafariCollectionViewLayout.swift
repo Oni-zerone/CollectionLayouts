e2a6170ffb090c92951c0cf907efaeb0036fd471
Andrea Altea <oni.zerone@gmail.com>
2019-06-01 14:25:56 +0200

    Feat: fix crashes on overscrolling

--- SafariLayout/Classes/SafariCollectionViewLayout.swift
+++ SafariLayout/Classes/SafariCollectionViewLayout.swift
@@ -7,12 +7,15 @@
 
 import UIKit
 
+@IBDesignable
 public class SafariCollectionViewLayout: UICollectionViewLayout {
 
-    private var _tabsCount: Int = 6
-    public var tabsCount: Int {
+    private var _tabsCount: Int = 4
+
+    @IBInspectable public var tabsCount: Int {
         set {
             _tabsCount = max(newValue, 1)
+            collectionView?.reloadData()
         }
         get {
             return _tabsCount
@@ -42,43 +45,72 @@ public class SafariCollectionViewLayout: UICollectionViewLayout {
     }
     
     override public func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
-        guard let collection = collectionView,
-            let dataSource = collection.dataSource else {
-                return nil
+        
+        let firstVisibleItem = self.firstVisibleItem
+        let lastVisibleItem = min(firstVisibleItem + tabsCount, itemsCount - 1)
+        if lastVisibleItem < firstVisibleItem {
+            let indexPath = IndexPath(item: lastVisibleItem, section: 0)
+            guard let cellAttributes = layoutAttributesForItem(at: indexPath) else { return nil }
+            return [cellAttributes]
         }
         
-        let cellsCount = dataSource.collectionView(collection, numberOfItemsInSection: 0)
-        let firstItem = max(Int(collection.contentOffset.y / (collection.bounds.height / CGFloat(_tabsCount))), 0)
-        
         var attributes = [UICollectionViewLayoutAttributes]()
-        (firstItem ..< min(firstItem + _tabsCount + 1, cellsCount)).forEach { index in
-            guard let cellAttributes = layoutAttributesForItem(at: IndexPath(item: index, section: 0)) else { return }
+        (firstVisibleItem ... lastVisibleItem).forEach { index in
+            let indexPath = IndexPath(item: index, section: 0)
+            guard let cellAttributes = layoutAttributesForItem(at: indexPath) else { return }
             attributes.append(cellAttributes)
         }
         return attributes
     }
     
     override public func layoutAttributesForItem(at indexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
-        guard let collection = collectionView else { return nil }
+        let collectionBounds = self.collectionBounds
+        let collectionOffset = self.collectionOffset
+        
         let attributes = SafariCollectionViewLayoutAttributes(forCellWith: indexPath)
         attributes.frame = CGRect(x: 0,
-                                  y: CGFloat(indexPath.item) * (collection.bounds.height / CGFloat(_tabsCount)),
-                                  width: collection.bounds.width,
-                                  height: collection.bounds.height)
+                                  y: CGFloat(indexPath.item) * (collectionBounds.height / CGFloat(tabsCount)),
+                                  width: collectionBounds.width,
+                                  height: collectionBounds.height)
         
-        let tilt = ((collection.contentOffset.y - attributes.frame.minY) / collection.bounds.height)
+        let tilt = ((collectionOffset.y - attributes.frame.minY) / collectionBounds.height)
         var perspective = CATransform3DIdentity
         perspective.m34 = -1/1000
         
         let rotation = CATransform3DRotate(perspective, (tilt * variationAngle) - defaultAngle, 1.0, 0.0, 0.0)
-        let translation = CATransform3DMakeTranslation(0.0, collection.bounds.height / -2, 0.0)
+        let translation = CATransform3DMakeTranslation(0.0, collectionBounds.height / -2, 0.0)
         attributes.transform3D = CATransform3DConcat(rotation, translation)
         attributes.anchorPoint = CGPoint(x: 0.5, y: 0.0)
-        
         return attributes
     }
     
     public override func shouldInvalidateLayout(forBoundsChange newBounds: CGRect) -> Bool {
         return true
+    }
+}
+
+extension SafariCollectionViewLayout {
+    
+    var itemsCount: Int {
+        guard let collection = collectionView,
+            let dataSource = collection.dataSource else {
+                return 0
+        }
+        return dataSource.collectionView(collection, numberOfItemsInSection: 0)
+    }
+
+    var firstVisibleItem: Int {
+        guard let collection = collectionView else {
+                return 0
+        }
+        return max(Int(collection.contentOffset.y / (collection.bounds.height / CGFloat(tabsCount))), 0)
+    }
+    
+    var collectionBounds: CGRect {
+        return self.collectionView?.bounds ?? .zero
+    }
+    
+    var collectionOffset: CGPoint {
+        return self.collectionView?.contentOffset ?? .zero
     }
 }
